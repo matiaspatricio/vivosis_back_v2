03f8dbc0daba7a0a39fa4094e45ffbc6c3be93b4
matiaspatricio <matiasnrpatricio@gmail.com>
2023-07-31 02:33:25 -0300

    first commit9

--- src/database/database.js
+++ src/database/database.js
@@ -1,27 +1,5 @@
-/*const Sequelize = require("sequelize");
-
-
-
-const sequelize = new Sequelize({
-    database: 'verceldb',
-    username: 'default',
-    password: 'kV7yrZg0aMmG',
-    host: 'ep-aged-sun-52900944-pooler.us-east-1.postgres.vercel-storage.com',
-    port: 5432, // Puerto predeterminado de PostgreSQL
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false // Si tienes el certificado, esto puede ser false, de lo contrario, configúralo en true
-      }
-    }
-  });
-
-
-  module.exports = sequelize;
-*/
 const Sequelize = require("sequelize");
-
+const pg = require('pg');
 
 
 const sequelize = new Sequelize({
@@ -30,15 +8,14 @@ const sequelize = new Sequelize({
     password: 'Kwgau0OykPsFezlqGtHF011NhtZcjN5x',
     host: 'dpg-cj0637b438irjjakpjs0-a.oregon-postgres.render.com',
     port: 5432, // Puerto predeterminado de PostgreSQL
-    dialect: 'postgres',
+    dialect: 'postgres',    
     dialectOptions: {
       ssl: {
         require: true,
         rejectUnauthorized: false // Si tienes el certificado, esto puede ser false, de lo contrario, configúralo en true
       }
     }
-  }, {
-    dialectModule: pg});
+  });
 
 
   module.exports = sequelize;
