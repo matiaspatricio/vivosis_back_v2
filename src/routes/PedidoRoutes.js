6a93bf8c5d3e9aa3a1cd32c20a0daa926187897d
matiaspatricio <matiasnrpatricio@gmail.com>
2024-03-22 23:56:25 -0300

    Agrego gets del dashboard

--- src/routes/PedidoRoutes.js
+++ src/routes/PedidoRoutes.js
@@ -1,8 +1,30 @@
 const express = require("express");
-const { getAllPedidos,createPedido, getPedidoById,updatePedido,deletePedido} = require("../controllers/PedidoController");
+const {
+    getAllPedidos,
+    getPedidosPendientes,
+    createPedido,
+    getPedidoById,
+    getPedidoByCliente,
+    updatePedido,
+    deletePedido,
+    getPedidosHoy,
+    getPedidosAyer,
+    getPedidosSemana,
+    getPedidosSemanaAnterior,
+    getPedidosMes
+  } = require("../controllers/PedidoController");
 const router = express.Router();
 
 router.route("/getallpedidos").get(getAllPedidos);
+router.route('/getpedidospendientes').get( getPedidosPendientes);
+router.route('/getpedidobycliente/:id_cliente').get(getPedidoByCliente);
+router.route('/getpedidosayer').get(getPedidosAyer);
+router.route('/getpedidossemana').get(getPedidosSemana);
+router.route('/getpedidossemanaanterior').get(getPedidosSemanaAnterior);
+router.route('/getpedidosmes').get(getPedidosMes);
+router.route('/getpedidoshoy').get(getPedidosHoy);
+
+
 router.route("/:id").get(getPedidoById).put(updatePedido).delete(deletePedido);
 router.route("/").post(createPedido);
 
